f2325aa54348cc3e048df9e96743b3942616adb5
rggan <rongabriel98@gmail.com>
2024-03-17 13:27:44 -0700

    completed requirements in v1 project plan

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:convert'; // for json decoding
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 import 'package:studypharm/firebase_options.dart'; // import for rootBundle
 
 void main() async {
@@ -10,9 +11,13 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(NavigatorApp());
-
-}
+  runApp(
+    ChangeNotifierProvider( // Provide ThemeNotifier to the widget tree
+      create: (context) => ThemeNotifier(),
+      child: NavigatorApp(),
+    ),
+  );
+} 
 
 class Drug {
   final String id;
@@ -34,13 +39,34 @@ class Drug {
   });
 }
 
+class ThemeNotifier extends ChangeNotifier {
+  ThemeMode _themeMode = ThemeMode.light;
+
+  ThemeMode get themeMode => _themeMode;
+
+  void setTheme(ThemeMode themeMode) {
+    _themeMode = themeMode;
+    notifyListeners();
+  }
+}
+
 final db = FirebaseFirestore.instance;
 
 class NavigatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue)),
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.white,
+        hintColor: Colors.blue,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.grey[900],
+        hintColor: Colors.white,
+      ),
+      themeMode: Provider.of<ThemeNotifier>(context).themeMode,
       home: Navigator(
         onGenerateRoute: (settings) {
           return MaterialPageRoute(
@@ -57,30 +83,53 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('StudyPharm'),
-        ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              addDrugData();
-              fetchDrugList(context); // Pass the context to fetchDrugList
-            },
-            child: const Text('Fetch Data'),
-          ),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('StudyPharm'),
+      ),
+      body: Center(
+        child: Column(
+          children:[
+            IconButton(
+              onPressed: () {
+                // Toggle between light and dark modes
+                ThemeMode currentMode = Theme.of(context).brightness == Brightness.light
+                    ? ThemeMode.dark
+                    : ThemeMode.light;
+                Provider.of<ThemeNotifier>(context, listen: false).setTheme(currentMode);
+              },
+              icon: const Icon(Icons.lightbulb_outline),
+            ),
+            const Text('Toggle Theme'),
+            ElevatedButton(
+              onPressed: () {
+                addDrugData(context);
+              },
+              child: const Text('Fetch Data'),
+            ),
+          ]
+        )
       ),
     );
   }
 
-  void addDrugData() async {
+  void addDrugData(BuildContext context) async {
     final drugsCollection = db.collection("drugs");
 
     try {
       QuerySnapshot querySnapshot = await drugsCollection.get();
       int drugCount = querySnapshot.size;
+      print(drugCount); // error checking
+
+      if (drugCount != 200){
+        // Delete the entire collection
+        await drugsCollection.get().then((snapshot) {
+          for (DocumentSnapshot doc in snapshot.docs) {
+            doc.reference.delete();
+          }
+          print('Collection deleted successfully');
+        });
+      }
       if (drugCount == 0) {
         final drugsString =
             await rootBundle.loadString('lib/top_200_drugs.json');
@@ -96,12 +145,14 @@ class MyApp extends StatelessWidget {
             "schedule": drug['schedule'] ?? '',
             "notes": drug['notes'] ?? '',
           };
-          drugsCollection.add(drugData);
+          final String brandName = drugData['brandName'];
+          // to set brandName as document ID and set drug data to it
+          drugsCollection.doc(brandName).set(drugData);
+          print('Data added success');
         });
-        print('Data added success');
-      } else {
-        print('Collection not empty');
-      }
+      }        
+      print(drugCount); // error checking
+      fetchDrugList(context);
     } catch (e) {
       print("Error loading JSON file: $e");
     }
@@ -110,7 +161,7 @@ class MyApp extends StatelessWidget {
   void fetchDrugList(BuildContext context) {
     db.collection('drugs').get().then(
       (querySnapshot) {
-        print("Successfully completed");
+        print("Fetching Drug List Successfully completed");
         List<Drug> drugsList = [];
 
         for (var docSnapshot in querySnapshot.docs) {
@@ -169,7 +220,11 @@ class DrugListPageState extends State <DrugListPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Drug List'),
+        title: const Text(
+          'Drug List', 
+          style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+
       ),
       body: Column(
         children: [
@@ -198,14 +253,23 @@ class DrugListPageState extends State <DrugListPage> {
                 itemCount: filteredDrugs.length,
                 itemBuilder: (context, index) {
                   Drug drug = filteredDrugs[index];
-                  return ListTile(
-                    title: Text(
-                      drug.brandName,
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    onTap: () {
-                      navigateToDrugDetails(context, drug);
-                    },
+                  return Container(
+                    margin: const EdgeInsets.all(2.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.blue, 
+                        width: 2.0),
+                      borderRadius: BorderRadius.circular(8.0)
+                      ),
+                    child: ListTile(
+                      title: Text(
+                        drug.brandName,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      onTap: () {
+                        navigateToDrugDetails(context, drug);
+                      },
+                    )
                   );
                 },
               ),
@@ -247,160 +311,159 @@ class DrugDetailsPage extends StatefulWidget {
 }
 
 
-class _DrugDetailsPageState extends State<DrugDetailsPage>{
+class _DrugDetailsPageState extends State<DrugDetailsPage> {
   late TextEditingController notesController;
 
   @override
   void initState() {
     super.initState();
-    // Check if there are existing notes
     if (widget.drug.notes != null && widget.drug.notes!.isNotEmpty) {
-      // If there are existing notes, set them
       notesController = TextEditingController(text: widget.drug.notes);
-    } 
-    else {
-      // If no existing notes, set the default text
+    } else {
       notesController = TextEditingController(text: '');
     }
   }
 
   @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            widget.drug.brandName,
-            style: const TextStyle(
-              fontSize: 30.0,
-              fontWeight: FontWeight.bold,
-            ),
+  Widget build(BuildContext context) {
+    bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
+    Color textColor = isDarkMode ? Colors.white : Colors.black;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          widget.drug.brandName,
+          style: const TextStyle(
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
           ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              RichText(
-                  text: TextSpan(
-                    style: const TextStyle(
-                      fontSize: 16.0,
-                      color: Colors.black, // Set your desired text color
-                    ),
-                    children: <TextSpan>[
-                      const TextSpan(
-                        text: 'Generic Name: ',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      TextSpan(text: '${widget.drug.genericName}'),
-                    ],
-                  ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            RichText(
+              text: TextSpan(
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: textColor,
                 ),
-                const SizedBox(height: 16),
-                RichText(
-                  text: TextSpan(
-                    style: const TextStyle(
-                      fontSize: 16.0,
-                      color: Colors.black, // Set your desired text color
+                children: <TextSpan>[
+                  const TextSpan(
+                    text: 'Generic Name: ',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
                     ),
-                    children: <TextSpan>[
-                      const TextSpan(
-                        text: 'Drug Class: ',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      TextSpan(text: '${widget.drug.drugClass}'),
-                    ],
                   ),
+                  TextSpan(text: '${widget.drug.genericName}'),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
+            RichText(
+              text: TextSpan(
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: textColor,
                 ),
-                const SizedBox(height: 16),
-                RichText(
-                  text: TextSpan(
-                    style: const TextStyle(
-                      fontSize: 16.0,
-                      color: Colors.black, // Set your desired text color
+                children: <TextSpan>[
+                  const TextSpan(
+                    text: 'Drug Class: ',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
                     ),
-                    children: <TextSpan>[
-                      const TextSpan(
-                        text: 'Indication: ',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      TextSpan(text: '${widget.drug.indication}'),
-                    ],
                   ),
+                  TextSpan(text: '${widget.drug.drugClass}'),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
+            RichText(
+              text: TextSpan(
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: textColor,
                 ),
-                const SizedBox(height: 16),
-                RichText(
-                  text: TextSpan(
-                    style: const TextStyle(
-                      fontSize: 16.0,
-                      color: Colors.black, // Set your desired text color
+                children: <TextSpan>[
+                  const TextSpan(
+                    text: 'Indication: ',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
                     ),
-                    children: <TextSpan>[
-                      const TextSpan(
-                        text: 'Schedule: ',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      TextSpan(text: getScheduleText(widget.drug.schedule)),
-                    ],
                   ),
+                  TextSpan(text: '${widget.drug.indication}'),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
+            RichText(
+              text: TextSpan(
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: textColor,
                 ),
-                const SizedBox(height: 16),
+                children: <TextSpan>[
+                  const TextSpan(
+                    text: 'Schedule: ',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextSpan(text: getScheduleText(widget.drug.schedule)),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
 
-              // NOTES SECTION
-              const Center(child: Text(
+            // NOTES SECTION
+            const Center(
+              child: Text(
                 'Notes',
                 style: TextStyle(
                   decoration: TextDecoration.underline,
                   fontSize: 18.0,
                   fontWeight: FontWeight.bold,
                 ),
-              ),),
-              const SizedBox(height: 8.0),
-              Container(
-                padding: const EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.blue,
-                    width: 3.0,
-                  ),
-                  borderRadius: BorderRadius.circular(8.0),
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            Container(
+              padding: const EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.blue,
+                  width: 3.0,
                 ),
-                child: TextField(
-                  controller: notesController,
-                  decoration: const InputDecoration(
-                    hintText: "Enter your notes here:",
-                    // border: InputBorder.none,
-                  ),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: TextField(
+                controller: notesController,
+                decoration: const InputDecoration(
+                  hintText: "Enter your notes here:",
+                  // border: InputBorder.none,
                 ),
               ),
-              const SizedBox(height: 16.0),
-              Center(
-                child: ElevatedButton(
-                  onPressed: saveNotes,
-                  style: ElevatedButton.styleFrom(
-                    textStyle: const TextStyle(fontSize: 18.0),
-                  ),
-                  child: const Text('Save Notes'),
+            ),
+            const SizedBox(height: 16.0),
+            Center(
+              child: ElevatedButton(
+                onPressed: saveNotes,
+                style: ElevatedButton.styleFrom(
+                  textStyle: const TextStyle(fontSize: 18.0),
                 ),
+                child: const Text('Save Notes'),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-      );
-    }
+      ),
+    );
+  }
 
-  // helper function to 
   String getScheduleText(String? schedule) {
     return schedule != null && schedule.isNotEmpty ? schedule : 'N/A';
-  } 
+  }
 
   void saveNotes() async {
     try {
@@ -408,12 +471,10 @@ class _DrugDetailsPageState extends State<DrugDetailsPage>{
         'notes': notesController.text,
       });
 
-      // Update the local state with the new notes
       setState(() {
         widget.drug.notes = notesController.text;
       });
 
-      // Show a confirmation message
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
           content: Text('Notes saved successfully'),
@@ -423,5 +484,4 @@ class _DrugDetailsPageState extends State<DrugDetailsPage>{
       print('Error saving notes: $e');
     }
   }
-
 }
